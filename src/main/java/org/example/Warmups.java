dfddbc000d6fa26071b8e3da992211ddf63a30fd
Zach Moore <sluggermoore@gmail.com>
2023-07-04 20:41:51 -0400

    functional test

--- src/main/java/org/example/Warmups.java
+++ src/main/java/org/example/Warmups.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Warmups {
 
     public static void main(String[] args) {
@@ -157,6 +160,23 @@ public class Warmups {
         }
         return false;
 
+    }
+
+
+    //FUNCTIONAL - LISTS
+    /*
+    Given a list of integers, return a list where each integer is multiplied with itself.
+
+    square([1, 2, 3]) → [1, 4, 9]
+    square([6, 8, -6, -8, 1]) → [36, 64, 36, 64, 1]
+    square([]) → []
+     */
+
+    public List<Integer> square(List<Integer> nums) {
+        nums = nums.stream()
+                .map(n -> n * n)
+                .collect(Collectors.toList());
+        return nums;
     }
 }
 
