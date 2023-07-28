f9b6dae8532b0f52e045a92bf935fffb108fe2bf
Zach Moore <sluggermoore@gmail.com>
2023-07-27 21:09:01 -0400

    fixTeen problem

--- src/main/java/org/example/Warmups.java
+++ src/main/java/org/example/Warmups.java
@@ -181,6 +181,17 @@ public class Warmups {
         return (a>21)? b : (b>21)? a : Math.max(a,b);
     }
 
+    /*
+    Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13..19 inclusive --
+    then that value counts as 0, except 15 and 16 do not count as a teens. Write a separate helper "public int fixTeen(int n)
+    {"that takes in an int value and returns that value fixed for the teen rule. In this way, you avoid repeating the teen code 3
+    times (i.e. "decomposition"). Define the helper below and at the same indent level as the main noTeenSum().
+
+    noTeenSum(1, 2, 3) → 6
+    noTeenSum(2, 13, 1) → 3
+    noTeenSum(2, 1, 14) → 3
+     */
+
 
     //FUNCTIONAL - LISTS
     /*
@@ -190,6 +201,29 @@ public class Warmups {
     square([6, 8, -6, -8, 1]) → [36, 64, 36, 64, 1]
     square([]) → []
      */
+
+    public int noTeenSum(int a, int b, int c) {
+        int sum = 0;
+
+        a = fixTeen (a);
+        b = fixTeen (b);
+        c = fixTeen (c);
+
+        sum = a + b + c;
+
+        return sum;
+
+    }
+
+    public int fixTeen (int n){
+        if (n == 15 || n == 16){
+            return n;
+        } else if (n >= 13 && n <= 19){
+            n = 0;
+            return n;
+        }
+        return n;
+    }
 
     public List<Integer> square(List<Integer> nums) {
         nums = nums.stream()
