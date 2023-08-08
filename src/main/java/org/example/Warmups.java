c157374479f8bea0a9a331208fe310d7aa3c0eba
Zach Moore <sluggermoore@gmail.com>
2023-08-07 21:52:54 -0400

    map2 problem

--- src/main/java/org/example/Warmups.java
+++ src/main/java/org/example/Warmups.java
@@ -98,7 +98,7 @@ public class Warmups {
         }
         return mapLen;
     }
-    
+
     /*
     Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring.
     So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
@@ -279,6 +279,29 @@ public class Warmups {
         return -1;
     }
 
+    /*
+    The classic word-count algorithm: given an array of strings, return a Map<String, Integer>
+    with a key for each different string, with the value the number of times that string appears in the array.
+
+    wordCount(["a", "b", "a", "c", "b"]) → {"a": 2, "b": 2, "c": 1}
+    wordCount(["c", "b", "a"]) → {"a": 1, "b": 1, "c": 1}
+    wordCount(["c", "c", "c", "c"]) → {"c": 4}
+     */
+
+    public Map<String, Integer> wordCount(String[] strings) {
+        Map<String, Integer> map = new HashMap<String, Integer> ();
+        for (String s:strings) {
+
+            if (!map.containsKey(s)) {
+                map.put(s, 1);
+            }
+            else {
+                int count = map.get(s);
+                map.put(s, count + 1);
+            }
+        }
+        return map;
+    }
 
     //FUNCTIONAL - LISTS
     /*
