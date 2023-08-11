3da94ff5b7ce23b353f9a767c963ed90bde3509e
Zach Moore <sluggermoore@gmail.com>
2023-08-10 21:34:08 -0400

    string 2 problem

--- src/main/java/org/example/Warmups.java
+++ src/main/java/org/example/Warmups.java
@@ -303,6 +303,27 @@ public class Warmups {
         return map;
     }
 
+    /*
+    A sandwich is two pieces of bread with something in between. Return the string that is between the first and
+    last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of bread.
+
+    getSandwich("breadjambread") → "jam"
+    getSandwich("xxbreadjambreadyy") → "jam"
+    getSandwich("xxbreadyy") → ""
+     */
+
+    public String getSandwich(String str) {
+        int firstBread = str.indexOf("bread");
+        int lastBread = str.lastIndexOf("bread");
+
+        if (firstBread == lastBread){
+            return "";
+        }
+
+        return str.substring(firstBread + 5, lastBread);
+    }
+
+
     //FUNCTIONAL - LISTS
     /*
     Given a list of integers, return a list where each integer is multiplied with itself.
